fd70fe7297385fe00d6fe3c1029a75a304d68fc2
NeeloByron <byronnkhuna@gmail.com>
2026-09-18 14:24:35 +0200

    Created middeware/logger and passed two build in middleware in the server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
+import bodyParser from "body-parser"
 import express, { Express } from "express"
 
 const app: Express = express()
 const PORT = process.env.PORT || 5000
+
+// two build in middleware essential for passing incoming json data
+app.use(express.json()) 
+app.use(bodyParser.json()) 
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
