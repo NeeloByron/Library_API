3c34a7a5cb4cdd297641430f29bf0236f84d205e
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-09-21 18:42:21 +0200

    increament on model for author & POST, PUT and DELETE Authors

--- src/models/authors-model.ts
+++ src/models/authors-model.ts
@@ -7,3 +7,10 @@ export interface Author {
 
 // storage for the authors 
 export const authors: Author[] = [];
+
+// it will start at 1 since the array starts empty
+let nextAuthorId = 1;
+
+export const getNextAuthorId = (): number => {
+    return nextAuthorId++;
+}
--- src/routes/authors-route.ts
+++ src/routes/authors-route.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { body, param, validationResult } from "express-validator";
-import { Author, authors } from '../models/authors-model';
+import { Author, authors, getNextAuthorId } from '../models/authors-model';
 
 const router = Router();
 
@@ -48,3 +48,60 @@ router.get("/:id", idValidation, (req: Request, res: Response) => {
 
     res.status(200).json({ success: true, data: author });
 });
+
+// POST /authors
+router.post("/", authorValidation, (req: Request, res: Response) => {
+     // run express-validator's checks 
+    if (sendValidationErrors(req, res))
+        return;
+
+    const newAuthor: Author = {
+        id: getNextAuthorId(),
+        authorName: req.body.authorName,
+        title: req.body.title,
+    };
+     // Persist to the in-memomery store
+    authors.push(newAuthor);
+     // 201 created 
+    res.status(201).json({ success: true, data: newAuthor });
+});
+
+// PUT /authors/:id
+router.put("/:id", [idValidation, ...authorValidation], (req: Request, res: Response ) => {
+    if (sendValidationErrors(req, res))
+        return;
+
+    const author = authors.find((item) => item.id === Number(req.params.id),);
+    if (!author) { res.status(404).json({ success: false, message: "Author not found"});
+       return;
+    }
+
+   author.authorName = req.body.authorName;
+    author.title = req.body.title;
+
+    res.status(200).json({
+      success: true,
+      data: author,
+    });
+  },
+);
+
+// DELET /authors/:id
+router.delete("/:id", idValidation, (req: Request, res: Response) => {
+    if (sendValidationErrors(req, res))
+        return;
+
+    const authorIndex = authors.findIndex(
+        (item) => item.id === Number(req.params.id) 
+    );
+
+    if (authorIndex === -1) {
+        res.status(404).json({ success: false, message: "Author not found"});
+        return;
+    }
+
+    const [deleteAuthor] = authors.splice(authorIndex, 1);
+    res.status(200).json({ success: true, message: "Author deleted successfully", data: deleteAuthor });
+});
+
+export default router;
